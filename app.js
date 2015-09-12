89f37e6b8e2202662357b2650e0c1f4aa0c55224
fbid <federico.bidoggia@gmail.com>
2015-09-12 12:31:37 +0200

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080; // Port 8080 if you run locally
+var address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
+
+// attivazione di EJS per la gestione del rendering
+app.engine('.html', require('ejs').renderFile);
+
+// logica di gestione delle richieste
+app.get('/',function(req,res){
+    res.render('index.html')
+});
+app.get('/about',function(req,res){
+    res.render('about.html');
+});
+
+var server = app.listen(port, address); 
